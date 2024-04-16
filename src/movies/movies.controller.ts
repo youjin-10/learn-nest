bb69005da212c842b210f5c9bc3a61093eec8327
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-16 13:59:09 +0900

    throwing exception in service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -44,9 +44,6 @@ export class MoviesController {
 
   @Patch('/:id')
   patch(@Param('id') id: string, @Body() updateData) {
-    return {
-      updatedMovie: id,
-      ...updateData,
-    };
+    return this.moviesService.update(id, updateData);
   }
 }
