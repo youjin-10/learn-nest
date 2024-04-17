2e9c0c1608fd347ec9bdfec72bb6c700f3dd853b
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-17 23:33:32 +0900

    Validation pipe as global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // pipe is a middleware that will be executed before the request handler
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // will remove any other properties that are not in the DTO
+      forbidNonWhitelisted: true, // will throw an error if there are properties that are not in the DTO
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
