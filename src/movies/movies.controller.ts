bb970f7f99683636a136bb68f16a5bbbfd1e5dc5
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-17 23:34:01 +0900

    dto & class-validator, decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,11 +6,11 @@ import {
   Param,
   Patch,
   Post,
-  Put,
   Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -21,29 +21,29 @@ export class MoviesController {
     return this.moviesService.getAll();
   }
 
-  // should be above the @Get('/:id') because it will be treated as a parameter
+  // should come before @Get('/:id') because it will be treated as a parameter
   @Get('/search')
   search(@Query('year') searchingYear: string) {
     return `searching for a movie made after: ${searchingYear}`;
   }
 
   @Get('/:id')
-  getOne(@Param('id') id: string): Movie {
+  getOne(@Param('id') id: number): Movie {
     return this.moviesService.getOne(id);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.moviesService.deleteOne(id);
   }
 
   @Patch('/:id')
-  patch(@Param('id') id: string, @Body() updateData) {
+  patch(@Param('id') id: number, @Body() updateData) {
     return this.moviesService.update(id, updateData);
   }
 }
